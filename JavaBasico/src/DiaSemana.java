4e3a72a59468f6282635eeabb1a213a753464077
Sc4rlxrd <guilhermesantos2004gss@gmail.com>
2023-02-15 12:31:39 -0300

    Updating my IDEA

--- JavaBasico/src/DiaSemana.java
+++ JavaBasico/src/DiaSemana.java
@@ -7,29 +7,14 @@ public class DiaSemana {
         System.out.println("Escolha um número que corresponde a um dia da semana");
         int numero = entrada.nextInt();
         switch (numero) {
-        case 1:
-            System.out.println("1. DOMINGO");
-        break;
-        case 2:
-            System.out.println("2. SEGUNDA - FEIRA");
-        break;
-        case 3:
-            System.out.println("3. TERÇA - FEIRA");
-        break;
-        case 4:
-            System.out.println("4. QUARTA - FEIRA");
-        break;
-        case 5:
-            System.out.println("5. QUINTA - FEIRA");
-        break;
-        case 6:
-            System.out.println("6. SEXTA - FEIRA");
-        break;
-        case 7:
-            System.out.println(" 7. SÁBADO");
-        break;
-        default:
-        System.out.println("O número escolhido é inválido! Digite um número entre 1 a 7.");
+            case 1 -> System.out.println("1. DOMINGO || Sunday");
+            case 2 -> System.out.println("2. SEGUNDA - FEIRA || Monday");
+            case 3 -> System.out.println("3. TERÇA - FEIRA || Tuesday");
+            case 4 -> System.out.println("4. QUARTA - FEIRA || Wednesday ");
+            case 5 -> System.out.println("5. QUINTA - FEIRA || Thursday ");
+            case 6 -> System.out.println("6. SEXTA - FEIRA || Friday");
+            case 7 -> System.out.println(" 7. SÁBADO || Saturday ");
+            default -> System.out.println("O número escolhido é inválido! Digite um número entre 1 a 7.");
         }
 
         }
--- JavaBasico/src/Main.java
+++ JavaBasico/src/Main.java
@@ -7,48 +7,45 @@ public class Main {
         double numero2;
         int menu;
         double resultado;
-        while(true){
-        System.out.print("Informe o primeiro número: ");
-        numero1 = scan.nextDouble();
-        System.out.print("Informe o segundo número: ");
-        numero2 = scan.nextDouble();
-        LinhaSeparacao.separacao();
-        System.out.print("Qual opção você deseja:");
-        System.out.println("\n1-Adição\n2-Subtração\n3-Multiplicação\n4-Divisão\n5-Resto da divisão\n6-Parar");
-        menu =scan.nextInt();
-        LinhaSeparacao.separacao();
-        switch(menu){
-            case 1:
-                resultado = Calculadora.somar(numero1, numero2);
-                System.out.println("A soma entre " + numero1 + " e " +  numero2 + " deu: " + resultado);
-                break;
-            case 2:
-                resultado =  Calculadora.subtracão(numero1, numero2);
-                System.out.println("A subtração entre " + numero1 + " e " + numero2 + " deu: " + resultado);
-                break;
-            case 3:
-                resultado = Calculadora.multiplicacao(numero1, numero2);
-                System.out.println("A multiplicação entre " + numero1 + " e " + numero2 + " deu " + resultado);
-                break;
-            case 4: 
-                resultado = Calculadora.divisao(numero1, numero2);
-                System.out.println("A divisão entre " + numero1 + " e " + numero2 + " deu " + resultado);
-                break;
-            case 5:
-                resultado = Calculadora.restoDivisao(numero1, numero2);
-                System.out.println("O resto da divisão  entre " + numero1 + " e " + numero2 + " deu "  + resultado);
-            case 6:
-                System.out.println("Estamos encerrando os processos.....");
-                System.out.println("Volte sempre!!");
-                break;
-            default:
-                System.out.println("Opção inválida... Digite uma opção válida");
-                break;
-            } 
-            if (menu == 6){
-                break;
-            }      
-        }
+        do {
+            System.out.print("Informe o primeiro número: ");
+            numero1 = scan.nextDouble();
+            System.out.print("Informe o segundo número: ");
+            numero2 = scan.nextDouble();
+            LinhaSeparacao.separacao();
+            System.out.print("Qual opção você deseja:");
+            System.out.println("\n1-Adição\n2-Subtração\n3-Multiplicação\n4-Divisão\n5-Resto da divisão\n6-Parar");
+            menu = scan.nextInt();
+            LinhaSeparacao.separacao();
+            switch (menu) {
+                case 1:
+                    resultado = Calculadora.somar(numero1, numero2);
+                    System.out.println("A soma entre " + numero1 + " e " + numero2 + " deu: " + resultado);
+                    break;
+                case 2:
+                    resultado = Calculadora.subtracão(numero1, numero2);
+                    System.out.println("A subtração entre " + numero1 + " e " + numero2 + " deu: " + resultado);
+                    break;
+                case 3:
+                    resultado = Calculadora.multiplicacao(numero1, numero2);
+                    System.out.println("A multiplicação entre " + numero1 + " e " + numero2 + " deu " + resultado);
+                    break;
+                case 4:
+                    resultado = Calculadora.divisao(numero1, numero2);
+                    System.out.println("A divisão entre " + numero1 + " e " + numero2 + " deu " + resultado);
+                    break;
+                case 5:
+                    resultado = Calculadora.restoDivisao(numero1, numero2);
+                    System.out.println("O resto da divisão  entre " + numero1 + " e " + numero2 + " deu " + resultado);
+                case 6:
+                    System.out.println("Estamos encerrando os processos.....");
+                    System.out.println("Volte sempre!!");
+                    break;
+                default:
+                    System.out.println("Opção inválida... Digite uma opção válida");
+                    break;
+            }
+        } while (menu != 6);
        
 
     }
